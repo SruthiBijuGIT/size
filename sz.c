001c5cee712006b22af769cfe8e8e3b9561a6db1
SruthiBijuGIT <128403226+SruthiBijuGIT@users.noreply.github.com>
2023-06-29 10:07:47 -0700

    Create sz.c

---
+++ sz.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+
+    int a;
+    float b;
+    char c;
+    double d;
+    printf("Size of int = %ld bytes\n", sizeof(a));
+    printf("Size of float = %ld bytes\n", sizeof(b));
+    printf("Size of char = %ld byte\n", sizeof(c));
+    printf("Size of double = %ld bytes\n", sizeof(d));
+    return 0;
+}
